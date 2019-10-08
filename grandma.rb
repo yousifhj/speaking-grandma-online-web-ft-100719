aa1ca9699b620a0dc82556ede0386fac12efb991
Huda Yousif <hjdoski@gmail.com>
2019-10-08 20:48:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,19 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-
-    if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
-      return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    else
-  		return "NO, NOT SINCE 1938!"
-    end
-
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 
