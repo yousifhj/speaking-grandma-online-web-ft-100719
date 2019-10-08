f4c23438c366c2d2a6e8aab0899c3a26aa223748
Huda Yousif <hjdoski@gmail.com>
2019-10-08 20:44:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  else phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
  end 
 end 
